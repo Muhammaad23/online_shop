7b27a9a18da4ac5565d512b9f5e0943bf482ddf6
Simply Done <muhammadmominov2004@gmail.com>
2025-04-24 18:27:48 +0500

    push

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .serializers import *
 # Create your views here.
+from .models import *
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class ProductView(APIView):
+    def get(self, request):
+        queryset = Product.objects.all()
+        serializers = ProductSerializer(queryset, many=True)
+        return Response(serializers.data)
